e4a50a05d7a937b93e444b2870473f06fdbf0af0
a-tasli <adatasli04@gmail.com>
2025-12-15 21:10:04 +0300

    modifications

--- server.py
+++ server.py
@@ -11,10 +11,12 @@ q_amount_label = ttk.Label(frm, text="# of questions").grid(column=1, row=2)
 path_label = ttk.Label(frm, text="Path of questions file").grid(column=1, row=4)
 
 port_entry = ttk.Entry(frm).grid(column=1, row=1)
-q_amount_entry = ttk.Entry(frm).grid(column=1, row=3)
-path_entry = ttk.Entry(frm).grid(column=1, row=5)
+q_amount_entry = ttk.Entry(frm).grid(column=1, row=3, columnspan=2)
+path_entry = ttk.Entry(frm).grid(column=1, row=5, columnspan=2)
 
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=7, columnspan=2)
+port_button = ttk.Button(frm, text="Host").grid(column=2, row=1)
+
+start_button = ttk.Button(frm, text="Start Game", command=root.destroy).grid(column=1, row=7, columnspan=2)
 root.mainloop()
 # port number entry
 # no of qs

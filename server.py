515328ec8a90d2858bf54d140ce068f2b09035ae
a-tasli <adatasli04@gmail.com>
2025-12-15 21:55:38 +0300

    server gui foundation

--- server.py
+++ server.py
@@ -2,21 +2,35 @@ from tkinter import *
 from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+frm.grid(sticky="NSEW")
 
-main_console = Listbox(frm).grid(column=0, row=0, rowspan=7)
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
 
-port_label = ttk.Label(frm, text="Port").grid(column=1, row=0)
-q_amount_label = ttk.Label(frm, text="# of questions").grid(column=1, row=2)
-path_label = ttk.Label(frm, text="Path of questions file").grid(column=1, row=4)
+frm.rowconfigure(0, weight=1)
+frm.rowconfigure(1, weight=1)
+frm.rowconfigure(2, weight=1)
+frm.rowconfigure(3, weight=1)
+frm.rowconfigure(4, weight=1)
+frm.rowconfigure(5, weight=1)
+frm.rowconfigure(6, weight=2)
+frm.columnconfigure(0, weight=8)
+frm.columnconfigure(1, weight=4)
+frm.columnconfigure(2, weight=1)
 
-port_entry = ttk.Entry(frm).grid(column=1, row=1)
-q_amount_entry = ttk.Entry(frm).grid(column=1, row=3, columnspan=2)
-path_entry = ttk.Entry(frm).grid(column=1, row=5, columnspan=2)
+main_console = Listbox(frm).grid(column=0, row=0, rowspan=7, sticky="NSEW")
 
-port_button = ttk.Button(frm, text="Host").grid(column=2, row=1)
+port_label = ttk.Label(frm, text="Port", anchor=W).grid(column=1, row=0, sticky="EW", padx=(6, 6))
+q_amount_label = ttk.Label(frm, text="# of questions", anchor=W).grid(column=1, row=2, sticky="W", padx=(6, 0))
+path_label = ttk.Label(frm, text="Path of questions file", anchor=W).grid(column=1, row=4, sticky="W", padx=(6, 0))
 
-start_button = ttk.Button(frm, text="Start Game", command=root.destroy).grid(column=1, row=7, columnspan=2)
+port_entry = ttk.Entry(frm).grid(column=1, row=1, sticky="NEW", padx=(6, 0))
+q_amount_entry = ttk.Entry(frm).grid(column=1, row=3, columnspan=2, sticky="NEW", padx=(6, 0))
+path_entry = ttk.Entry(frm).grid(column=1, row=5, columnspan=2, sticky="NEW", padx=(6, 0))
+
+port_button = ttk.Button(frm, text="Host").grid(column=2, row=1, sticky="NEW", pady=(3, 0))
+
+start_button = ttk.Button(frm, text="Start Game", command=root.destroy).grid(column=1, row=6, columnspan=2, sticky="NSEW", padx=(6, 0), pady=(6, 0))
 root.mainloop()
 # port number entry
 # no of qs

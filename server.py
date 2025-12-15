54c403465f855bc129edaf80277efcbdaa7b3eb1
a-tasli <adatasli04@gmail.com>
2025-12-15 21:02:40 +0300

    server ui init

---
+++ server.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+main_console = Listbox(frm).grid(column=0, row=0, rowspan=7)
+
+port_label = ttk.Label(frm, text="Port").grid(column=1, row=0)
+q_amount_label = ttk.Label(frm, text="# of questions").grid(column=1, row=2)
+path_label = ttk.Label(frm, text="Path of questions file").grid(column=1, row=4)
+
+port_entry = ttk.Entry(frm).grid(column=1, row=1)
+q_amount_entry = ttk.Entry(frm).grid(column=1, row=3)
+path_entry = ttk.Entry(frm).grid(column=1, row=5)
+
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=7, columnspan=2)
+root.mainloop()
+# port number entry
+# no of qs
+# question filename
+# start button
+# list box

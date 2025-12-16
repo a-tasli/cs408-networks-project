b8c7d696cc682be981971b18326b719d7f64d3c5
a-tasli <adatasli04@gmail.com>
2025-12-16 21:00:47 +0300

    ui bug fix

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import scrolledtext
+from ui_helpers import *
 
 def start_button_func():
     print(port_entry.get())
@@ -43,15 +44,20 @@ q_amount_label = ttk.Label(frame, text="# of questions", anchor=W).grid(column=1
 path_label = ttk.Label(frame, text="Path to questions file", anchor=W).grid(column=1, row=4, sticky="W", padx=(6, 0))
 
 # entry boxes
-port_entry = ttk.Entry(frame).grid(column=1, row=1, sticky="NEW", padx=(6, 0))
-q_amount_entry = ttk.Entry(frame).grid(column=1, row=3, columnspan=2, sticky="NEW", padx=(6, 0))
-path_entry = ttk.Entry(frame).grid(column=1, row=5, columnspan=2, sticky="NEW", padx=(6, 0))
+port_entry = ttk.Entry(frame)
+port_entry.grid(column=1, row=1, sticky="NEW", padx=(6, 0))
+
+q_amount_entry = ttk.Entry(frame)
+q_amount_entry.grid(column=1, row=3, columnspan=2, sticky="NEW", padx=(6, 0))
+
+path_entry = ttk.Entry(frame)
+path_entry.grid(column=1, row=5, columnspan=2, sticky="NEW", padx=(6, 0))
 
 # host button
 host_button = ttk.Button(frame, text="Host").grid(column=2, row=1, sticky="NEW", pady=(3, 0))
 
 # start game button
-start_button = ttk.Button(frame, text="Start Game", command=start_button_func).grid(column=1, row=6, columnspan=2, sticky="NSEW", padx=(6, 0), pady=(6, 0))
+start_button = ttk.Button(frame, text="Start Game", command=lambda: print_to_box(main_console, "zart zurt")).grid(column=1, row=6, columnspan=2, sticky="NSEW", padx=(6, 0), pady=(6, 0))
 
 # start it up
 root.mainloop()
--- ui_helpers.py
+++ ui_helpers.py
@@ -1,4 +1,9 @@
-def print_to_box(box : ScrolledText, a : string):
-    box.configure(state = "enabled")
-    box.insert(a)
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
+import string
+
+def print_to_box(box : scrolledtext.ScrolledText, a : string):
+    box.configure(state = "normal")
+    box.insert("end", a)
     box.configure(state = "disabled")

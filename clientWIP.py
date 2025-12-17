1f40bf540c60d6f2f0680747c59bf15a9cbd05e2
eyelightnment <an.alara.barut@gmail.com>
2025-12-17 17:26:53 +0300

    todo comments

--- clientWIP.py
+++ clientWIP.py
@@ -9,11 +9,14 @@ def receive_messages(client_socket):
             if message:
                 print(f"\n[SERVER]: {message}")
             else:
+                # graceful close
                 break
         except:
             print("[ERROR] Connection lost.")
+            # error close
             break
 
+# TODO make these function
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 12345))
 
--- serverWIP.py
+++ serverWIP.py
@@ -3,25 +3,30 @@ import threading
 
 def handle_client(client_socket, address):
     print(f"[NEW CONNECTION] {address} connected.")
+    # TODO probably keep a list of clients here
     while True:
         try:
             # Receive message from this specific client
             message = client_socket.recv(1024).decode()
             if not message:
+                # graceful close
                 break
             print(f"[{address}] {message}")
         except:
+            # error close
             break
     
     client_socket.close()
     print(f"[DISCONNECTED] {address} closed.")
 
+# TODO make this function
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 12345))
 server.listen()
 
 print("[LISTENING] Server is looking for connections...")
 
+# TODO do this with thread
 while True:
     # Accept new connections in a loop
     conn, addr = server.accept()
